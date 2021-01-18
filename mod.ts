2673e5746da3b1b153513f4746f50e8c65fdb79d
ezarcel <ezarcel05@gmail.com>
2021-01-18 20:25:27 +0100

    first commit

---
+++ mod.ts
@@ -0,0 +1,18 @@
+export class LazyGetter<T> {
+	protected result: T | null = null
+
+	constructor(
+		target: { [ key: string ]: any },
+		name: string,
+		protected callback: () => T
+	) {
+		Object.defineProperty(target, name, {
+			get: (): T => {
+				if (this.result === null) this.result = callback()
+				return this.result
+			}
+		})
+	}
+}
+
+export default LazyGetter
